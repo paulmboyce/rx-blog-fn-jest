fb59dc82079281c8fd13741d05a2fac841b4f59d
paulmboyce <paul.boyce@goobito.com>
2020-12-01 13:27:35 +0000

    feat(redux-thunk): install redux-thunk and wire up with redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 
 import App from "./components/App";
 import { reducePosts } from "./reducers";
 const reducers = combineReducers({
 	posts: reducePosts,
 });
-const store = createStore(reducers);
+const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
 	<Provider store={store}>
