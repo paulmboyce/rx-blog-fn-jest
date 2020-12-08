93e07f95da9ffde91e75b2e6d2e3aa4cde869476
paulmboyce <paul.boyce@goobito.com>
2020-12-08 13:20:56 +0000

    refactor(userheader): break dependency on authors as a prop
    use mapStateToProps to extract the single author via (state, ownProps)

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -2,21 +2,19 @@ import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { getOneAuthorAction } from "../actions/AuthorsAction";
 
-const UserHeader = ({ dispatch, id, authors }) => {
+const UserHeader = ({ dispatch, id, author }) => {
 	useEffect(() => {
 		dispatch(getOneAuthorAction(id));
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
-	const author = authors.find((a) => a.id === id);
 	if (!author) {
 		return null;
 	}
-
-	return <h5 className="ui header blue">{author && author.name}</h5>;
+	return <h5 className="ui header blue">{author.name}</h5>;
 };
 
-const mapStateToProps = (state) => {
-	return { authors: state.authors };
+const mapStateToProps = (state, ownProps) => {
+	return { author: state.authors.find((a) => a.id === ownProps.id) };
 };
 export default connect(mapStateToProps)(UserHeader);
