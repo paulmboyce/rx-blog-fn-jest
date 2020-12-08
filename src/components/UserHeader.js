425683773995b6a36ec1215f16d4e3c0002bc158
paulmboyce <paul.boyce@goobito.com>
2020-12-08 12:52:34 +0000

    refactor(userheader): simplify code by removing useState and inlining get author

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,19 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { getOneAuthorAction } from "../actions/AuthorsAction";
 
 const UserHeader = ({ dispatch, id, authors }) => {
-	const [author, setAuthor] = useState(null);
-
 	useEffect(() => {
-		const author = authors.find((a) => a.id === id);
-		if (author) {
-			setAuthor(author);
-		} else {
-			dispatch(getOneAuthorAction(id));
-		}
+		dispatch(getOneAuthorAction(id));
 		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [authors]);
+	}, []);
+
+	const author = authors.find((a) => a.id === id);
+	if (!author) {
+		return null;
+	}
 
 	return <h5 className="ui header blue">{author && author.name}</h5>;
 };
